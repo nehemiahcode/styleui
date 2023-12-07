38750e684157a5473d32e2d02b0e3624b14c6047
Nehemiah <codehubby@gmail.com>
2023-12-07 21:06:08 +0100

    Update Section1.jsx

--- components/Introduction/Section1.jsx
+++ components/Introduction/Section1.jsx
@@ -22,7 +22,7 @@ export default function Section1() {
           className=" h-auto object-cover w-auto"
         />
       </div>
-      <div className=" border-b-1 border-slate-600 my-3 lg:w-[50%] sm:w-[80%] ">
+      <div className=" border-b-1 px-1 border-slate-600 my-3 lg:w-[50%] sm:w-[80%] ">
         <h1
           id="what-is-styleui"
           onClick={() => router.push("#what-is-styleui")}
@@ -30,20 +30,20 @@ export default function Section1() {
         >
           What is StyleUI?
         </h1>
-        <p className=" text-white py-3 text-base dark:text-black">
+        <p className="text-white py-3 text-sm dark:text-black">
           StyleUI is a UI library for React that helps you build beautiful and
           accessible user interfaces. Created on top of Tailwind CSS and React
           Aria, which also works in hand with other Ui component libraries to
           speed up customization.
         </p>
-        <p className="  text-white py-3 text-base dark:text-black">
+        <p className=" text-white py-3 text-sm dark:text-black">
           StyleUI&apos;s primary goal is to streamline the development process,
           offering a beautiful and adaptable system design for an enhanced user
           experience.
         </p>
       </div>
 
-      <div className=" my-3 lg:w-[50%]  sm:w-[80%]">
+      <div className=" my-3 lg:w-[50%] px-2 sm:w-[80%]">
         <h1
           id="how-is-styleui-related-to-tailwindcss"
           onClick={() => router.push("#how-is-styleui-related-to-tailwindcss")}
@@ -55,7 +55,7 @@ export default function Section1() {
           {" "}
           <li>TailwindCSS:</li>
         </ul>
-        <p className="  text-white py-3 text-base dark:text-black">
+        <p className="  text-white py-3 text-sm dark:text-black">
           Tailwind CSS is a CSS Framework that provides atomic CSS classes to
           help you style components, leaving you to handle lots of other things
           like accessibility, component composition, keyboard navigation, style
@@ -65,7 +65,7 @@ export default function Section1() {
         <ul className=" text-sky-900 font-bold  text-lg">
           <li>StyleUI:</li>
         </ul>
-        <p className="  text-white py-3 text-base dark:text-black">
+        <p className="  text-white py-3 text-sm dark:text-black">
           StyleUi is a UI library for React that combines the power of
           TailwindCSS with React and other external UI libraries to provide a
           complete components &bracket;logic and styles&bracket; for building accessible and
@@ -75,7 +75,7 @@ export default function Section1() {
         </p>
       </div>
 
-      <div className=" my-3 lg:w-[50%]  sm:w-[80%]">
+      <div className=" my-3 lg:w-[50%] px-2  sm:w-[80%]">
         <h1
           id="does-style-ui-use-runtime-css?"
           onClick={() => router.push("#does-style-ui-use-runtime-css?")}
@@ -83,7 +83,7 @@ export default function Section1() {
         >
           Does StyleUI use runtime CSS?
         </h1>
-        <p className="  text-white py-3 text-base dark:text-black">
+        <p className="  text-white py-3 text-sm dark:text-black">
           No, As StyleUI uses TailwindCSS as its style engine, it generates CSS
           at build time, eliminating the need for runtime CSS. This means that
           StyleUI is fully compatible with the latest React and Next.js
@@ -98,7 +98,7 @@ export default function Section1() {
           Can I use StyleUi with other front-end frameworks or libraries, such as
           Vue or Angular?
         </h1>
-        <p id="2" className="  text-white py-3 text-base dark:text-black">
+        <p id="2" className="  text-white py-3 text-sm dark:text-black">
           No, StyleUI is specifically designed for React as it is built on top of
           React Aria. However, you can still use the NextUI components styling
           part with other frameworks or libraries.
